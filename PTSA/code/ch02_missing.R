2bbe3c6fca51aadd01086e4b5ff7a438457672cb
Stephen CUI <junocuixuan@163.com>
2024-03-18 22:58:33 +0800

    缺失值

---
+++ PTSA/code/ch02_missing.R
@@ -0,0 +1,31 @@
+require(zoo) # zoo provides time series functionality
+require(data.table) # data.table is a high performance data frame
+
+unemp <- fread("data/UNRATE.csv")
+unemp[, DATE := as.Date(DATE)]
+setkey(unemp, DATE)
+
+# generate a data set where data is randomly missing
+rand.unemp.idx <- sample(1:nrow(unemp), .1 * nrow(unemp))
+rand.unemp <- unemp[-rand.unemp.idx]
+
+# generate a data set where data is more likely to be missing if it's high
+high.unemp.idx <- which(unemp$UNRATE > 8)
+num.to.select <- .5 * length(high.unemp.idx)
+high.unemp.idx <- sample(high.unemp.idx, num.to.select)
+bias.unemp <- unemp[-high.unemp.idx]
+
+
+all.dates <- seq(from = unemp$DATE[1], to = tail(unemp$DATE, 1), by = "months")
+rand.unemp <- rand.unemp[J(all.dates), roll = 0]
+bias.unemp <- bias.unemp[J(all.dates), roll = 0]
+rand.unemp[, rpt := is.na(UNRATE)]
+# here we label the missing data for easy plotting
+
+
+rand.unemp[, impute.ff := zoo::na.locf(UNRATE, na.rm = FALSE)]
+bias.unemp[, impute.ff := zoo::na.locf(UNRATE, na.rm = FALSE)]
+
+unemp[350:400, plot(DATE, UNRATE, col = 1, lwd = 2, type = "b")]
+rand.unemp[350:400, lines(DATE, impute.ff, col = 2, lwd = 2, lty = 2)]
+rand.unemp[350:400][rpt == TRUE, points(DATE, impute.ff, col = 2, pch = 6, cex = 2)]
