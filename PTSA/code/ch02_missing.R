f8c4166a926fa026553c3ff6b6bb36c481e78180
Stephen CUI <junocuixuan@163.com>
2024-03-20 23:00:34 +0800

    移动平均填充

--- PTSA/code/ch02_missing.R
+++ PTSA/code/ch02_missing.R
@@ -28,4 +28,41 @@ bias.unemp[, impute.ff := zoo::na.locf(UNRATE, na.rm = FALSE)]
 
 unemp[350:400, plot(DATE, UNRATE, col = 1, lwd = 2, type = "b")]
 rand.unemp[350:400, lines(DATE, impute.ff, col = 2, lwd = 2, lty = 2)]
-rand.unemp[350:400][rpt == TRUE, points(DATE, impute.ff, col = 2, pch = 6, cex = 2)]
+rand.unemp[350:400][
+  rpt == TRUE,
+  points(DATE, impute.ff, col = 2, pch = 6, cex = 2)
+]
+
+png("img/forward_fill_plot.png")
+unemp[350:400, plot(DATE, UNRATE, col = 1, type = "b")]
+rand.unemp[350:400, lines(DATE, impute.ff, col = 2)]
+rand.unemp[350:400][rpt == TRUE, points(DATE, impute.ff, col = 2, lwd = 3)]
+dev.off()
+
+rand.unemp[, impute.rm.nolookahead := rollapply(
+  c(NA, NA, UNRATE), 3,
+  function(x) {
+    if (!is.na(x[3])) x[3] else mean(x, na.rm = TRUE)
+  }
+)]
+bias.unemp[, impute.rm.nolookahead := rollapply(
+  c(NA, NA, UNRATE), 3,
+  function(x) {
+    if (!is.na(x[3])) x[3] else mean(x, na.rm = TRUE)
+  }
+)]
+
+rand.unemp[, complete.rm := rollapply(
+  c(NA, UNRATE, NA), 3,
+  function(x) {
+    if (!is.na(x[2])) {
+      x[2]
+    } else {
+      mean(x, na.rm = TRUE)
+    }
+  }
+)]
+
+png("img/moving_average_plot.png")
+use.idx <- 150:200
+unemp[use.idx, plot(DATE, UNRATE), col = 1, type = "b"]
